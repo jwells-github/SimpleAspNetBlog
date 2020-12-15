c1caa19586289d8b86a153217e308bf066abe598
JakeW <jakewellsd@gmail.com>
2020-12-15 21:49:43 +0000

    Add BlogPost Model

---
+++ SimpleBlog/SimpleBlog/Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleBlog.Models
+{
+    public class BlogPost
+    {
+        public long BlogPostID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PostedDate { get; set; }
+    }
+}
